b1b3dd069048e73bdc0147ade88403c76927f894
Dubwastaken <asherwelch@gmail.com>
2024-12-03 19:52:34 -0800

    Add files via upload

---
+++ Shape.py
@@ -0,0 +1,4 @@
+print ("   /|")
+print ("  / |")
+print (" /hi|")
+print ("/___|")
---
+++ variabletest.py
@@ -0,0 +1,6 @@
+character_name = "John"
+character_age = 30
+is_male = True
+print(character_name, " is ", character_age)
+character_name = "Steve"
+print("name is now changed to ", character_name)
